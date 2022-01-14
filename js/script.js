e0fa6a70c5b2b64f883ff1d416a025fb97877df2
lean-13 <leandrodavid13@hotmail.com>
2022-01-14 16:46:32 -0300

    class y constructor

--- js/script.js
+++ js/script.js
@@ -1,83 +1,58 @@
-// mensajes
 
 let autor = console.log ("El autor de esta pagina es Goñi Leandro");
 
 alert("Bienvenido a Music House")
 
+// aceptar terminos y condiciones
 let terminos;
 while (terminos != "y") {
     terminos = prompt ("Ingrese Y para aceptar terminios y condiciones").toLowerCase()
 };
 alert("Gracias por aceptar terminos y condiciones");
-// dolar
-const DOLAR = 104;
+// iva
+const IVA = 1.21;
+// envio gratis
+const ENVIOGRATIS = 40;
+
 // productos
-let guitarra = 329;
-let piano = 474;
-let bateria = 949;
-let producto;
-let catalogoProductos = prompt("elija el producto que quiere consultar. Guitarra, Piano, Bateria").toLowerCase();
-switch (catalogoProductos) {
+class Product{
+
+    constructor(nombre,marca,precio){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    calcularEnvio(){
+        if (this.precio > ENVIOGRATIS) {
+            this.envio = 0;
+        }
+        else {
+            this.envio = 800;
+        }
+    }
+
+    mostrarProducto(){
+        alert("Producto: " + this.nombre + "\n" + "Marca: " + this.marca + "\n" + "Precio: " + this.precio + " Dolares" + "\n" + "Envio: " + this.envio + " pesos");
+    }
+}
+// productos
+const GUITARRA = new Product("GUITARRA CRIOLLA CLÁSICA","ALPUJARRA","329");
+const PIANO = new Product("PIANO ELÉCTRICO","ARTESIA","474");
+
+// catalogo
+let seleccionProductos = prompt("elija el producto que quiere consultar. Guitarra, Piano, Bateria").toLowerCase();
+switch (seleccionProductos) {
     case "guitarra":
-        producto = guitarra;
-        alert("GUITARRA CRIOLLA CLÁSICA LA ALPUJARRA 80. precio" + " " + producto + " " + "dolares")
-        conversionPesos()
-        preguntaCuotas()
+        GUITARRA.calcularEnvio();
+        GUITARRA.mostrarProducto();
         break;
     case "piano":
-        producto = piano;
-        alert("PIANO ELÉCTRICO ARTESIA PERFORMER. precio" + " " + producto + " " + "dolares")
-        conversionPesos()
-        preguntaCuotas()
-        break;
-    case "bateria":
-        producto = bateria;
-        alert("BATERIA MAPEX PRODIGY. precio" + " " + producto + " " + "dolares")
-        conversionPesos()
-        preguntaCuotas()
+        PIANO.calcularEnvio();
+        PIANO.mostrarProducto();
         break;
     default:
-        alert("opcion no valida")
+        alert("Producto no valido")
         break;
 }
-// cambio de moneda
-function conversionPesos() {
-    let precioDolar = prompt ("Para convertir el precio a pesos ingrese: y").toLowerCase()
-    if (precioDolar == "y") {
-        precioProducto = DOLAR * producto;
-        alert(precioProducto + " " + "pesos argentinos")
-    }
-}
-// cuotas
-function preguntaCuotas() {
-    let preguntaCuotas = prompt("Para calcular cuotas ingrese: y").toLowerCase()
-    if (preguntaCuotas == "y") {
-        seleccionCuotas()
-    }
-}
-function seleccionCuotas() {
-    let cuotas = prompt("desea cuotas en dolares o pesos?")
-    if ((cuotas == "dolares") || (cuotas == "dolar")) {
-        cuotasDolar()
-    }
-    else if ((cuotas == "pesos") || (cuotas == "peso")) {
-        cuotasPesos()
-    }
-}
-function cuotasDolar() {
-    let cuotas = prompt("cuantas cuotas desea?")
-    let calcularCuotas = producto / cuotas;
-    alert(cuotas + " " +"cuotas de" + " " + calcularCuotas + " " + "dolares");
-}
-function cuotasPesos() {
-    let precioProducto = DOLAR * producto;
-    let cuotas = parseInt(prompt("cuantas cuotas desea?"))
-    let calcularCuotas = precioProducto / cuotas;
-    alert(cuotas + " " +"cuotas de" + " " + calcularCuotas + " " + "pesos");
-}
- 
 
-
-
-
-    

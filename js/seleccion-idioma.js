e233ad7be5d501584c2bbce9014e907637a85c2c
lean-13 <leandrodavid13@hotmail.com>
2022-01-23 23:48:58 -0300

    seleccion idioma en index y localstorage

--- js/seleccion-idioma.js
+++ js/seleccion-idioma.js
@@ -1,17 +1,24 @@
 // seleccion de idioma español o ingles
-
+const seleccion = document.querySelector(".bienvenida--modificacion");
 const seleccionIdioma = ()=>{
     document.querySelector(".es").addEventListener("click",()=>{
         localStorage.setItem("idioma","es");
+        cerrarSeleccionIdioma()
     })    
     document.querySelector(".en").addEventListener("click",()=>{
         localStorage.setItem("idioma","en");
+        cerrarSeleccionIdioma();
     })
 }
 
+const cerrarSeleccionIdioma = ()=>{
+    seleccion.style.animation = "desaparecer 1s forwards";
+    setTimeout(()=>seleccion.style.display = "none",1000)
+}
 const idioma = localStorage.getItem("idioma");
 
 if (idioma ===  null) seleccionIdioma();
 else {
     console.log(`idioma: ${idioma}`);
+    seleccion.style.display = "none";
 }

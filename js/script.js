bbf693e705984c38e3a5b10bd7e0a83243372609
lean-13 <leandrodavid13@hotmail.com>
2022-01-26 11:29:35 -0300

    compra de un producto

--- js/script.js
+++ js/script.js
@@ -1,39 +1,9 @@
-/*
+
 // iva
 const IVA = 1.21;
 // envio gratis
 const ENVIOGRATIS = 40;
 
-// productos
-class Producto{
-
-    constructor(nombre,marca,precio){
-        this.id = id;
-        this.nombre = nombre;
-        this.marca = marca;
-        this.precio = precio;
-    }
-
-    calcularEnvio(){
-        if (this.precio > ENVIOGRATIS) {
-            this.envio = 0;
-        }
-        else {
-            this.envio = 800;
-        }
-    }
-    sumaIva(){
-        this.precio = this.precio * IVA;
-    }
-
-    mostrarProducto(){
-        alert("Producto: " + this.nombre + "\n" + "Marca: " + this.marca + "\n" + "Precio: " + this.precio + " Dolares" + "\n" + "Envio: " + this.envio + " pesos");
-    }
-}
-
-// productos
-const GUITARRA = new Producto("GUITARRA CRIOLLA CLÁSICA","ALPUJARRA","329");
-const PIANO = new Producto("PIANO ELÉCTRICO","ARTESIA","474");
 
 // array carrito
 
@@ -45,7 +15,6 @@ function agregarCarrito() {
     productoCarrito = "GUITARRA CRIOLLA CLÁSICA";
     carrito.push("\n" + productoCarrito);
     posicionProducto();
-    alert(numeroPosicionProducto + "-" + productoCarrito + " agregado al carrito con exito");
 }
 
 // contar productos
@@ -54,7 +23,7 @@ function posicionProducto() {
         numeroPosicionProducto = index + 1;
     }
 }
-*/
+
 
 
 let modalAbrir = document.querySelectorAll(".botonCompra")[0];
@@ -62,6 +31,7 @@ let modalCerrar = document.querySelectorAll(".botonCerrar")[0];
 let modalCompra = document.querySelectorAll(".modalCompra")[0];
 
 modalAbrir.addEventListener("click", function(e) {
+    agregarCarrito()
     e.preventDefault();
     modalCompra.style.opacity = "1";
     modalCompra.style.visibility = "visible";

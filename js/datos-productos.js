7d309023ff3af4951a1a6a2a3336940d02875f97
lean-13 <leandrodavid13@hotmail.com>
2022-01-27 20:14:12 -0300

    base de datos productos

---
+++ js/datos-productos.js
@@ -0,0 +1,16 @@
+
+// productos
+[
+    {
+        "id": 1,
+        "nombre": "Guitarra Criolla Clasica",
+        "marca": "Alpujarra",
+        "precio": 34.000
+    },
+    {
+        "id": 2,
+        "nombre": "Guitarra Electrica EG001",
+        "marca": "Femmto Stratocaster",
+        "precio": 190000
+    }
+]

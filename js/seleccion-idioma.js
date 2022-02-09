9e89109e884c098efcc14a297a87f04787f30677
lean-13 <leandrodavid13@hotmail.com>
2022-02-09 16:33:24 -0300

    seleccion idioma jquery

--- js/seleccion-idioma.js
+++ js/seleccion-idioma.js
@@ -1,24 +1,31 @@
-// seleccion de idioma español o ingles
-const seleccion = document.querySelector(".modal--modificacion");
-const seleccionIdioma = ()=>{
-    document.querySelector(".es").addEventListener("click",()=>{
-        localStorage.setItem("idioma","es");
-        cerrarSeleccionIdioma()
-    })    
-    document.querySelector(".en").addEventListener("click",()=>{
-        localStorage.setItem("idioma","en");
-        cerrarSeleccionIdioma();
-    })
-}
 
-const cerrarSeleccionIdioma = ()=>{
-    seleccion.style.animation = "desaparecer 1s forwards";
-    setTimeout(()=>seleccion.style.display = "none",1000)
-}
-const idioma = localStorage.getItem("idioma");
+$(document).ready(function () {
+    // seleccion de idioma español o ingles
+    let seleccion = $('#modalSeleccionIdioma');
+    const seleccionIdioma = () => {
+        $(".es").on("click",() => {
+            localStorage.setItem("idioma","es");
+            cerrarSeleccionIdioma()
+        })    
+        $(".en").on("click",() => {
+            localStorage.setItem("idioma","en");
+            cerrarSeleccionIdioma();
+        })
+    }
 
-if (idioma ===  null) seleccionIdioma();
-else {
-    console.log(`idioma: ${idioma}`);
-    seleccion.style.display = "none";
-}
+    const cerrarSeleccionIdioma = ()=>{
+        seleccion.fadeOut();
+    }
+    const idioma = localStorage.getItem("idioma");
+
+    if (idioma ===  null) {
+        seleccionIdioma();
+    }
+    else {
+        console.log(`idioma: ${idioma}`);
+        $(seleccion).fadeOut();
+    }
+
+
+});
+

b754c9be23647d7619d5b6902ff0f0dfb3b7d2e7
lean-13 <leandrodavid13@hotmail.com>
2022-02-15 17:22:58 -0300

    modalCarrito animado

---
+++ js/modalCarrito.js
@@ -0,0 +1,21 @@
+// modal carrito
+$(document).ready(function () {
+    let modalCarrito = $('.modalCarrito');
+    let modalCarritoSeccion = $('.modalCarritoSeccion');
+    let botonModal = $('.botonCompra');
+
+    $(botonModal).on('click', () => {
+        mostrarModalCarrito();
+    })
+    $('.modalCarrito').on('click', () => {
+        cerrarModalCarrito();
+    })
+    const mostrarModalCarrito = () => {
+        modalCarrito.fadeIn(800);
+        modalCarritoSeccion.animate({top:'60px'},700)
+    }
+    const cerrarModalCarrito = ()=>{
+        modalCarrito.fadeOut(800);
+        modalCarritoSeccion.animate({top:'-500px'},700);
+    }
+});

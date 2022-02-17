a2326a236248ad9c4aa91f75d55b995aac85af7c
lean-13 <leandrodavid13@hotmail.com>
2022-02-16 21:00:36 -0300

    reinicio de precio

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,9 @@
 
 
 const vaciarCarrito = document.getElementById('botonVaciarCarrito')
-
+const reinicionPrecio = document.querySelector('.precio__total__numero');
 vaciarCarrito.addEventListener('click', () => {
     carrito = {};
     pintarCarrito();
+    reinicionPrecio.textContent = "$ " + "0000"
 })

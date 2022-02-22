1657f7567a9839adc5038592493b46d8d11f5834
lean-13 <leandrodavid13@hotmail.com>
2022-02-22 16:05:03 -0300

    boton modal compra

--- js/carrito.js
+++ js/carrito.js
@@ -2,6 +2,9 @@
 
 const vaciar = document.getElementById('botonVaciarCarrito')
 const reinicionPrecio = document.querySelector('.precio__total__numero');
+
+const selectCuotas = document.querySelector('.imputSeleccionCuotas');
+
 vaciar.addEventListener('click', () => {
     vaciarCarrito();
 })
@@ -10,4 +13,17 @@ const vaciarCarrito = () => {
     carrito = {};
     pintarCarrito();
     reinicionPrecio.textContent = "$ " + "0000"
+}
+
+
+selectCuotas.addEventListener('change', (event) => {
+    const cantidadCuotas = document.getElementById('infoNumeroCuotas');
+    cantidadCuotas.textContent = "Cuotas: " + event.target.value;
+    numeroCuotas = event.target.value;
+})
+
+function calcularPrecioCuotas() {
+    let precioCuotas = precioProductoFinal / numeroCuotas;
+    const infoPrecioCuotas = document.getElementById('infoPrecioCuotas')
+    infoPrecioCuotas.textContent = "Cuotas de: $" + precioCuotas;
 }
